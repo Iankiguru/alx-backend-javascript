36257f9c1dab436d581e6a5aa8e0bd93da7a1f63
Ian Kiguru <iankiguru72@outlook.com>
2024-01-03 12:17:58 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-const handleProfileSignup = () => {
-  Promise.all([uploadPhoto(), createUser()])
+function handleProfileSignup() {
+  Promise.all([uploadPhoto('photo-profile-1'), createUser()])
     .then(([photo, user]) => {
       console.log(`Body: ${user.firstName} ${user.lastName}`);
     })
     .catch((error) => {
       console.error('Signup system offline');
     });
-};
-
+}
 export default handleProfileSignup;
