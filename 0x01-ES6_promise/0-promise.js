97103a64f9e58c34e2fe209bd84de9e254267c6f
Ian Kiguru <iankiguru72@outlook.com>
2024-01-03 12:08:56 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,8 +2,6 @@ const apiCall = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("Data from API");
-      // Uncomment the line below if you want to simulate an error response
-      // reject("Error fetching data from API");
     }, 2000);
   });
 };
