cd81805c224b7cd1451c052e4f74c44d2aaeabae
Ian Kiguru <iankiguru72@outlook.com>
2024-01-04 21:32:20 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,7 @@
-// 1-make_classrooms.js
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
     const roomSizes = [19, 20, 34];
     return roomSizes.map(size => new ClassRoom(size));
 }
-
 export default initializeRooms;
