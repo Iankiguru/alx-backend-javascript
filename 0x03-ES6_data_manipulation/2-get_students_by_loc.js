ac5e168600c51e6a0407f7ff176ce47f1799ac02
Ian Kiguru <iankiguru72@outlook.com>
2024-01-08 12:59:17 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,11 +1,7 @@
-// 2-get_students_by_loc.js
-
 function getStudentsByLocation(studentList, city) {
   // Use filter to select students located in the specified city
   const filteredStudents = studentList.filter(student => student.location === city);
 
   return filteredStudents;
 }
-
 export default getStudentsByLocation;
-
