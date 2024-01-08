12cb45d4f540cc8ee6c26cf8a0077ae0ce0c8c7b
Ian Kiguru <iankiguru72@outlook.com>
2024-01-08 13:00:52 +0300

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,11 +1,7 @@
-// 7-has_array_values.js
-
 function hasValuesFromArray(set, arr) {
   // Check if all elements in the array exist within the set
   const allValuesExist = arr.every(value => set.has(value));
 
   return allValuesExist;
 }
-
 export default hasValuesFromArray;
-
