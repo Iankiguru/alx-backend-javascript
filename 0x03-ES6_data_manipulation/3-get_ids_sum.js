30e2307615ea7f32b0f6149142166f817f7203d3
Ian Kiguru <iankiguru72@outlook.com>
2024-01-08 12:59:34 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,11 +1,7 @@
-// 3-get_ids_sum.js
-
 function getStudentIdsSum(studentList) {
   // Use reduce to calculate the sum of student ids
   const sumOfIds = studentList.reduce((sum, student) => sum + student.id, 0);
 
   return sumOfIds;
 }
-
 export default getStudentIdsSum;
-
